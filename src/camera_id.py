196595111628c8078adeb33081fe16ff097fa62a
Eric Schmitt <eschmitt88@gmail.com>
2024-02-07 22:28:19 -0500

    function to write a label on an image

--- src/camera_id.py
+++ src/camera_id.py
@@ -37,9 +37,27 @@ def id_from_text(text, typ='int'):
 def screenshot_to_ID(image, typ='int', show=False):
     return id_from_text(OCR_image(crop_image(image, id_box), show=show), typ=typ)
 
+
+def write_id_to_image(image, string):
+    """Write the string to the image in the upper left."""
+    if isinstance(image, Image.Image):
+        image = np.array(image)
+    font_height = int(image.shape[0] / 5)
+    fscale = image.shape[0] / 150
+    line_width = int(image.shape[0] / 50)
+    cv2.putText(image, string, (0, font_height), cv2.FONT_HERSHEY_DUPLEX, fscale, (255, 255, 255), line_width)
+    return image
+
+
 if __name__ == "__main__":
     image = cv2.imread("tests/examples/camera_01.png")
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     start = time.time()
     print('ID:', screenshot_to_ID(image, show=False))
     print('OCR time:', time.time()-start)
+
+    image = cv2.imread("tests/examples/camera_45.png")
+    image = write_id_to_image(image, 'Camera 45')
+    cv2.imshow('labeled image', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
